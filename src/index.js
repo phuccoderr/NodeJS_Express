a378b9cb4e3c4888111e3378c25c095af19420c0
phuccoderr <phuctapcode@gmail.com>
2024-05-17 00:42:20 +0700

    install prettier,lint-staged,husky

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const path = require('path');
-const express = require('express');
-const { engine } = require('express-handlebars');
-const morgan = require('morgan');
+const path = require("path");
+const express = require("express");
+const { engine } = require("express-handlebars");
+const morgan = require("morgan");
 
 const app = express();
 const port = 3000;
-const route = require('./routes'); // Auto index !!!
+const route = require("./routes"); // Auto index !!!
 
 // Input Form Data
 app.use(
@@ -16,14 +16,14 @@ app.use(
 app.use(express.json());
 
 // Static File
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 // Template Engineer
-app.engine('hbs', engine({ extname: '.hbs' }));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.engine("hbs", engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
 
 // Middleware Logger
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 // Local Host -- Hosting
 // Action ----> Dispatcher ----> Function Handler
